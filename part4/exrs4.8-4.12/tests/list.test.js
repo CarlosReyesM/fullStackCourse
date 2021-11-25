754668f1289b2e64f7382bdaf15d94dca78db39a
Carlos Reyes Muñoz <41906847+CarlosReyesM@users.noreply.github.com>
2021-11-25 14:38:29 +0200

    create test helper

--- part4/exrs4.8-4.12/tests/list.test.js
+++ part4/exrs4.8-4.12/tests/list.test.js
@@ -1,55 +1,5 @@
 const listHelper = require('../utils/list_helper')
-
-const blogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
+const testHelper = require('../utils/testHelper')
 
 test('dummy returns one', () => {
   const result = listHelper.dummy()
@@ -62,12 +12,12 @@ test('no blogs', () => {
 })
 
 test('summa of total likes', () => {
-  const result = listHelper.totalLikes(blogs)
+  const result = listHelper.totalLikes(testHelper.blogs)
   expect(result).toBe(36)
 })
 
 test('get favorite blog', () => {
-  const result = listHelper.favoriteBlog(blogs)
+  const result = listHelper.favoriteBlog(testHelper.blogs)
   expect(result).toEqual({
     title: 'Canonical string reduction',
     author: 'Edsger W. Dijkstra',
@@ -76,12 +26,12 @@ test('get favorite blog', () => {
 })
 
 test('gete most blogs', () => {
-  const result = listHelper.mostBlogs(blogs)
+  const result = listHelper.mostBlogs(testHelper.blogs)
   expect(result).toEqual({ author: 'Robert C. Martin', blogs: 3 })
 })
 
 test('gete most liked', () => {
-  const result = listHelper.mostLikes(blogs)
+  const result = listHelper.mostLikes(testHelper.blogs)
   expect(result).toEqual({
     author: 'Edsger W. Dijkstra',
     likes: 17
